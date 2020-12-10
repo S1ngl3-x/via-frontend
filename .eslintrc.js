dc9c11815d518b566d1c1b184bc67793505b5796
lipowada <lipowada@fel.cvut.cz>
2020-12-10 15:55:52 +0100

    :wrench: Make all eslint error only warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
   ],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'only-warn'],
   // add your custom rules here
   rules: {},
 };
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,5 @@
 export default function ({ $axios, redirect }) {
   $axios.onRequest((config) => {
-    // eslint-disable-next-line no-console
-    console.log('Making request to ' + config.url);
     config.withCredentials = true;
     return config;
   });
